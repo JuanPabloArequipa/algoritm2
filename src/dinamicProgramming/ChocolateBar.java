f20237ef4a9f99d904bef287a1868212b963c7b4
JuanPabloArequipa <juan.arequipa.12833847@usalesiana.edu.bo>
2024-04-03 11:00:01 -0400

    Update ChocolateBar.java

--- src/dinamicProgramming/ChocolateBar.java
+++ src/dinamicProgramming/ChocolateBar.java
@@ -7,11 +7,13 @@ public class ChocolateBar {
         System.out.println("Sample 1");
         int input1 = 8;
         System.out.println(greedySolution(input1));
+        System.out.println(dpSolution(input1));
 
         System.out.println("Sample 2");
         int input2 = 12;
         System.out.println(greedySolution(input2));
-        
+        System.out.println(dpSolution(input2));
+
     }
 
     public static int greedySolution(int N){
@@ -25,5 +27,16 @@ public class ChocolateBar {
         }
 
         return pieces;
+    }
+
+    public static int dpSolution(int N) {
+        int[] dp = new int[N+1];
+        dp[0] = 1;
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.max(dp[i], dp[i-j] + 1);
+            }
+        }
+        return dp[N]/3;
     }
 }
